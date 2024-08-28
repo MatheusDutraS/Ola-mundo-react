9599689ec26d004d0d595b198f7e778f2ba2d822
Matheus Dutra de Souza <107437365+MatheusDutraS@users.noreply.github.com>
2024-08-27 22:20:14 -0300

    Adicionando arquivos

---
+++ src/componentes/Menu/index.js
@@ -0,0 +1,15 @@
+import styles from "./menu.module.css";
+import MenuLink from "../MenuLink";
+
+function Menu() {
+  return (
+    <header>
+      <nav className={styles.navegacao}>
+        <MenuLink to="/">Início</MenuLink>
+        <MenuLink to="/sobremim">Sobre mim</MenuLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Menu;
